b825404744c10857b89538c4755b0953b9212a22
Kenneth Martinsson <kema.martinsson@gmail.com>
2025-11-27 19:23:27 +0100

    Fix to resolve CI-pipeline failure

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -137,15 +137,20 @@ android {
         }
     }
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_1_8
-        targetCompatibility = JavaVersion.VERSION_1_8
+        sourceCompatibility = JavaVersion.VERSION_11
+        targetCompatibility = JavaVersion.VERSION_11
     }
     kotlinOptions {
-        jvmTarget = "1.8"
+        jvmTarget = "11"
     }
     buildFeatures {
         compose = true
         buildConfig = true
+    }
+    testOptions {
+        unitTests.all {
+            it.reports.junitXml.required.set(true)
+        }
     }
 }
 
