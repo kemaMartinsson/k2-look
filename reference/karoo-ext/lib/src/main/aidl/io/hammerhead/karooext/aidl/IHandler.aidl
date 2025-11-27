15cfd0bab313e86e68a9491e7fadbfa63ed59c08
Kenneth Martinsson <kema.martinsson@gmail.com>
2025-11-27 20:38:44 +0100

    Update gitignore

--- reference/karoo-ext/lib/src/main/aidl/io/hammerhead/karooext/aidl/IHandler.aidl
+++
@@ -1,7 +0,0 @@
-package io.hammerhead.karooext.aidl;
-
-interface IHandler {
-     void onNext(in Bundle bundle);
-     oneway void onError(String msg);
-     oneway void onComplete();
-}
--- reference/karoo-ext/lib/src/main/kotlin/io/hammerhead/karooext/models/KarooInfo.kt
+++
@@ -1,9 +0,0 @@
-package io.hammerhead.karooext.models
-
-import kotlinx.serialization.Serializable
-
-@Serializable
-data class KarooInfo(
-    val serial: String = "unknown",
-    val hardwareType: HardwareType = HardwareType.UNKNOWN,
-)
